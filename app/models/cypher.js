b601d38d1337959f9dab6ab09ab80bdf83d4ac1c
Brett Wilcox <wilcox.brett@gmail.com>
2020-11-29 15:50:16 -0700

    Update.

--- app/models/cypher.js
+++ app/models/cypher.js
@@ -14,7 +14,7 @@ export default class BlockCypher {
       hash: ${this.hash} </br>
       time: ${this.time} </br>
       <a href="https://live.blockcypher.com/btc/block/${this.hash}" target="_blank" class="mt-2 explorers">Block Cypher</a> </br>
-      <a href="https://blockstream.info/block/${this.hash}?expand" target="_blank" class="mt-2 explorers">Block Stream Explorer</a> </br>
+      <a href="https://blockstream.info/block/${this.hash}?expand" target="_blank" class="mt-2 explorers">Block Stream</a> </br>
       <a href="https://sochain.com/block/BTC/${this.hash}" target="_blank" class="mt-2 explorers">SoChain</a>
           `
   }
