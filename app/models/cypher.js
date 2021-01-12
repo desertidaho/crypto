787e987799408e5d77da84b94c879f251faf142c
Brett Wilcox <wilcox.brett@gmail.com>
2021-01-12 14:39:01 -0700

    Typo

--- app/models/cypher.js
+++ app/models/cypher.js
@@ -11,7 +11,7 @@ export default class BlockCypher {
     this.dateTime = this.timeSince(this.time.split('.')[0])
     return `
     <a href="https://sochain.com/block/BTC/${this.height}" target="" class="blockData">Bitcoin Block Height ${this.height} </a></br>
-    <a href="https://live.blockcypher.com/btc/block/${this.hash}" target="" class="mt-2 explorers">Block Cypher</a> </br>
+    <a href="https://live.blockcypher.com/btc/block/${this.hash}" target="" class="mt-2 explorers">BlockCypher</a> </br>
     <a href="https://blockstream.info/block/${this.hash}?expand" target="" class="mt-2 explorers">Blockstream</a> </br>
     <a href="https://medium.com/coinmonks/the-biggest-ultimate-2020-cryptocurrency-resource-list-300-resources-4523e47f298" class="mt-2 explorers">Resources</a> </br>
     <a href="https://www.g2.com/categories/cryptocurrency-exchanges" class="mt-2 explorers">Exchanges</a> </br>
