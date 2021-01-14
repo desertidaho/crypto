6b1b9512b3725a65ee1cf7ba9aac320b4ece09f8
Brett Wilcox <wilcox.brett@gmail.com>
2021-01-13 17:15:52 -0700

    Fix marketCap

--- app/models/marketCap.js
+++ app/models/marketCap.js
@@ -4,12 +4,12 @@ export default class MarketCap {
   }
 
   marketCapTemplate() {
-    console.log(this.marketCap.length)
-    if (this.marketCap.length > 18) {
-      this.marketCap = '$' + this.marketCap.slice(0, 2) + '.' + this.marketCap.slice(3, 5) + 'T' //$10.00T
+    this.marketCap = (this.marketCap.split('.'))[0].toString()
+    if (this.marketCap.length > 14) {
+      this.marketCap = '$' + this.marketCap.slice(0, 2) + '.' + this.marketCap.slice(2, 4) + 'T' //$10.00T
     }
-    else if (this.marketCap.length > 17) {
-      this.marketCap = '$' + this.marketCap.slice(0, 1) + '.' + this.marketCap.slice(2, 4) + 'T' //$1.00T
+    else if (this.marketCap.length == 13) {
+      this.marketCap = '$' + this.marketCap.slice(0, 1) + '.' + this.marketCap.slice(1, 3) + 'T' //$1.00T
     }
     else {
       this.marketCap = '$' + this.marketCap.slice(0, 3) + '.' + this.marketCap.slice(4, 6) + 'B' //$999.99B
